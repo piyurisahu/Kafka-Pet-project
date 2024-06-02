2abc0116ea0ff8858499880a17f2e1fd84de7aaf
unknown <Piyuri Sahu>
2024-06-01 18:59:33 -0700

    [60]:- Added kafka producer and docker configuration to create local kafka cluster

---
+++ kafka-producer-wikimedia/src/main/java/com/example/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,42 @@
+package com.example.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //create producer pro
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        String topic = "wikimedia.recentChange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+
+        EventSource eventSource = builder.build();
+
+        //Start the producer in another thread
+
+        eventSource.start();
+
+        //we produce for 10 min and block the program until then
+
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
